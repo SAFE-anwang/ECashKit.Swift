0f975dc3c86cc82f37103a6e0035b06f3fbdd412
JiangSonglun <271398741@qq.com>
2024-03-14 19:11:13 +0800

    update package "BitcoinCore.Swift" to 2.2.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/SAFE-anwang/BitcoinCore.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/SAFE-anwang/BitcoinCore.Swift.git", .upToNextMajor(from: "2.2.0")),
         .package(url: "https://github.com/SAFE-anwang/BitcoinCashKit.Swift.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
     ],
